ec0cf95e95fe60cb0117a842f41fd51cbdb06963
Max Riepkin <ryepkin.maksym98@gmail.com>
2018-02-06 14:26:41 +0200

    login and signup is done

--- public/javascripts/redirections.js
+++ public/javascripts/redirections.js
@@ -1,4 +1,12 @@
-$("#add").click(function () {
-    window.location.href = "/newuser";
-
+$('#add_user').on('click',function () {
+    $.ajax({
+        type:'POST',
+        url: '/',
+        success:function (err,res) {
+            if(!err){
+                console.log("REDIRECT");
+                res.redirect('/newuser')
+            }
+        }
+    })
 })
--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,50 @@ var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
 
-
+var nameOfRegisteredUser="";
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: nameOfRegisteredUser });
 });
 
+router.get("/login",function (req,res) {
+    res.render('login',{title:'Login page'});
+})
 
+router.post('/log',function (req,res) {
+    var MongoClient = mongodb.MongoClient;
+
+    var url = 'mongodb://localhost:27017/startup';
+
+    MongoClient.connect(url,function (err,client) {
+        if(err){
+            console.log("Cannot connect to db");
+        }else {
+            console.log("Connected");
+            var db=client.db('startup');
+            var collection = db.collection("users");
+
+            collection.find({login:req.body.login,password:req.body.password}).toArray(function(err, results) {
+                if(err){
+                    console.log("You are not registered")
+                }
+
+                else if(results.length){
+
+
+                    console.log(nameOfRegisteredUser);
+                    res.redirect('/')
+                }
+                else {
+                    console.log("Not reg");
+
+                    res.redirect('/login');
+                }
+                client.close()
+            })
+        }
+    })
+})
 
 router.get('/removeuser',function (req,res) {
     res.render('removeuser',{title:'Delete user'});
@@ -88,8 +125,6 @@ router.post('/adduser',function (req,res) {
 
             var collection = db.collection("users");
 
-
-
             collection.insertOne({user:req.body.user , login: req.body.login, password: req.body.password},function (err,result) {
                 if(err){
                     console.log("Cannot add new student to database",err);
@@ -98,6 +133,37 @@ router.post('/adduser',function (req,res) {
                     res.redirect("userlist");
                 }
                 client.close()
+            });
+        }
+    })
+})
+
+router.get('/edituser',function (req,res) {
+    res.render('edituser',{title:"Edit user"});
+})
+
+router.get('/editinguser',function (req,res) {
+    var MongoClient = mongodb.MongoClient;
+
+    var url = 'mongodb://localhost:27017/startup';
+
+    MongoClient.connect(url,function (err,client) {
+        if(err){
+            console.log("Unable to connect database",err);
+        }else{
+            console.log("Connected");
+
+            var db = client.db("startup");
+
+            var collection = db.collection("users");
+
+            collection.updateOne({user:req.body.user, login:req.body.login, password:req.body.password},function (err,result) {
+                if(err){
+                    console.log("Cannot make a change in database",err)
+                }else{
+                    res.redirect('/userlist');
+                }
+                client.close();
             });
         }
     })
