bc6729dae299120c6fd78632bf30f4f14c7c284f
Max Riepkin <ryepkin.maksym98@gmail.com>
2018-03-06 13:59:21 +0200

    splited router app

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var bodyParser = require('body-parser');
 var mongo = require('mongodb');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var user = require('./routes/user');
+var login = require('./routes/login');
+
 var app = express();
 
 // view engine setup
@@ -24,7 +26,8 @@ app.use(cookieParser());
 app.use(express.static(__dirname + '/public'));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/user', user);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
